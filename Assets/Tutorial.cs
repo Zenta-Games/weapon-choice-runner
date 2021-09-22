f201f1c19985d26b18e177a9f792f078662ee5f7
onurbas <onurbas@zenta.games>
2021-09-22 15:50:03 +0300

    Add finger tutorial

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenta.Core.Runtime.Managers;
+
+public class Tutorial : MonoBehaviour
+{
+    private void Start()
+    {
+        if (GameManager.Instance.Level == 0)
+        {
+            Destroy(this.gameObject,3f);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
